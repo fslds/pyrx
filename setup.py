97165c96634b5897142a01d0382f151d18ba7db9
Alex Humphreys <alex@advancedtelematic.com>
2015-06-24 10:19:05 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
 readme = open('readme.md').read()
-setup(name='PyRx',
+setup(name='pyrx-ats',
       version='0.3.0',
-      #author='Philip Schleihauf',
-      #author_email='uniphil@gmail.com',
+      #author='Philip Schleihauf, ATS Advanced Telematic Systems',
       license='GPLv2',
-      description='Rx schema and validation system',
+      description='Rx schema and validation system, with added error messages',
       long_description=readme,
-      url='https://github.com/uniphil/pyrx',
+      url='https://github.com/advancedtelematic/pyrx',
       py_modules=['pyrx'])
